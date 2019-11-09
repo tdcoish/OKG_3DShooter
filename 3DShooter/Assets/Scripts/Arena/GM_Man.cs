1dba21aca5c2b99249c7f293346c91bad5e7e516
Timothy Coish <tdcoish@gmail.com>
2019-11-09 10:24:13 -0800

    Capsule Running

---
+++ 3DShooter/Assets/Scripts/Arena/GM_Man.cs
@@ -0,0 +1,20 @@
+﻿/*************************************************************************************
+
+*************************************************************************************/
+using UnityEngine;
+
+public class GM_Man : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;    
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- 3DShooter/Assets/Scripts/Arena/PC_Cont.cs
+++ 3DShooter/Assets/Scripts/Arena/PC_Cont.cs
@@ -5,13 +5,86 @@ using UnityEngine;
 
 public class PC_Cont : MonoBehaviour
 {
+    private Rigidbody                   cRigid;
+    private PC_Cam                      cCam;
+
+    public float                        _accRate = 100f;
+    public float                        _maxSpd = 10f;
+
+
     void Start()
     {
-        
+        cRigid = GetComponent<Rigidbody>();
+        cCam = GetComponentInChildren<PC_Cam>();
     }
 
     void Update()
     {
-        
+        // want to get rid of y component of the cameras rotation.
+        float mouseX = Input.GetAxis("Mouse X") * 1f;
+        float mouseY = Input.GetAxis("Mouse Y") * 1f;
+        // look sensitivity between 0.5 - 1.5
+
+        // rotate camera view around y axis.
+        transform.RotateAround(transform.position, Vector3.up, mouseX);
+        Vector3 xAx = Vector3.Cross(transform.forward, Vector3.up);
+        cCam.transform.RotateAround(transform.position, xAx, mouseY);
+
+    }
+
+    void FixedUpdate()
+    {
+        cRigid.velocity = FindVelFromInput(cRigid.velocity);
+    }
+
+    private Vector3 FindVelFromInput(Vector3 vCurSpd)
+    {
+        float fSideAcc = 0f;
+        float fForAcc = 0f;
+
+        if(Input.GetKey(KeyCode.A)){
+            fSideAcc -= _accRate * Time.fixedDeltaTime;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            fSideAcc += _accRate * Time.fixedDeltaTime;
+        }
+        if(Input.GetKey(KeyCode.W)){
+            fForAcc += _accRate * Time.fixedDeltaTime;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            fForAcc -= _accRate * Time.fixedDeltaTime;
+        }
+
+        if(Mathf.Abs(fForAcc) + Mathf.Abs(fSideAcc) > _accRate)
+        {
+            fForAcc *= 0.707f;
+            fSideAcc *= 0.707f;
+        }
+
+
+        Vector3 vVel = vCurSpd;
+        // basically, if we're not accelerating, then make our velocity lowered.
+        // other wise do so normally.
+        if(Mathf.Abs(fForAcc) + Mathf.Abs(fSideAcc) < 0.1f)
+        {
+            // say we'll stop over 1 second, or so.
+            vVel -= vVel * Time.fixedDeltaTime * _maxSpd;
+            // and if we go too far, then just set vel to zero.
+            if(Vector3.Dot(vVel, vCurSpd) <= 0f)
+            {
+                return Vector3.zero;
+            }
+        }
+        else
+        {
+            vVel += fForAcc * transform.forward;
+            vVel += fSideAcc * transform.right;
+        }
+        if(Vector3.Magnitude(vVel) > _maxSpd)
+        {
+            vVel *= _maxSpd / Vector3.Magnitude(vVel);
+        }
+
+        return vVel;
     }
 }
