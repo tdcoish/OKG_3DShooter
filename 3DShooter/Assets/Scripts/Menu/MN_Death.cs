d24b8e9b702ba78c007e7d8278586afba5b4bddb
Timothy Coish <tdcoish@gmail.com>
2019-11-09 13:41:18 -0800

    Death Screen

---
+++ 3DShooter/Assets/Scripts/Menu/MN_Death.cs
@@ -0,0 +1,27 @@
+﻿/*************************************************************************************
+
+*************************************************************************************/
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MN_Death : MonoBehaviour
+{
+    public Text                         _txtScore;
+
+
+    void Start()
+    {
+        _txtScore.text = "SCORE: " + 1000;
+    }
+
+    public void BT_PlayAgain()
+    {
+        SceneManager.LoadScene("Arena");        
+    }
+    public void BT_MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
