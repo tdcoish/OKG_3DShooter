aa3915e6b481916289ffc93c6303351e7a91e7eb
Timothy Coish <tdcoish@gmail.com>
2019-11-09 12:07:10 -0800

    Pickups
    Work in progress.

---
+++ 3DShooter/Assets/Scripts/Arena/PCK_Hlth.cs
@@ -0,0 +1,12 @@
+﻿/*************************************************************************************
+
+*************************************************************************************/
+using UnityEngine;
+
+public class PCK_Hlth : PCK_Base
+{
+    protected override void PickupEvent()
+    {
+        TDC_EventManager.FBroadcast(TDC_GE.GE_PCK_HLT);
+    }
+}
