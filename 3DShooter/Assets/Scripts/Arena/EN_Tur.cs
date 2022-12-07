fdf5f6e64f26abc1185d67c99089635327a85fad
Timothy Coish <tdcoish@gmail.com>
2022-12-06 20:17:19 -0800

    First push after a long time

--- 3DShooter/Assets/Scripts/Arena/EN_Tur.cs
+++ 3DShooter/Assets/Scripts/Arena/EN_Tur.cs
@@ -3,6 +3,8 @@
 *************************************************************************************/
 using UnityEngine;
 
+public enum STATE_TUR {SFIRING, SCOOLDOWN}
+
 public class EN_Tur : EN_Base
 {
 
@@ -10,10 +12,26 @@ public class EN_Tur : EN_Base
 
     private float                       _lastShot;
     public float                        _shotInterval = 2f;
+    private float                       _coolDownStartTime;
+    public float                        _coolDownTime = 2f;
+    public int                          _volleyAmount = 5;
+    public int                          _volleyCurShot = 0;
+
+    public STATE_TUR                    _state = STATE_TUR.SFIRING;
     public WP_FirePoint                 rFirePoint;
 
     void Update()
     {
+        switch(_state)
+        {
+            case STATE_TUR.SFIRING: RUN_FIRING_STATE(); break;
+            case STATE_TUR.SCOOLDOWN: RUN_COOLDOWN_STATE(); break;
+        }
+    }
+
+    void RUN_FIRING_STATE()
+    {
+        // Eventually want to make the turret have rotational speed. For now just snap to player.
         Vector3 vDir = rPC.transform.position - transform.position;
         vDir.y = 0f;
         vDir = Vector3.Normalize(vDir);
@@ -26,6 +44,21 @@ public class EN_Tur : EN_Base
             vDir = rPC.transform.position - rFirePoint.transform.position;
             b.FFireDirection(vDir);
             _lastShot = Time.time;
+
+            _volleyCurShot++;
+            if(_volleyCurShot >= _volleyAmount)
+            {
+                _state = STATE_TUR.SCOOLDOWN;
+                _coolDownStartTime = Time.time;
+            }
+        }
+    }
+
+    void RUN_COOLDOWN_STATE()
+    {
+        if(Time.time - _coolDownStartTime > _coolDownTime){
+            _state = STATE_TUR.SFIRING;
+            _volleyCurShot = 0;
         }
     }
 }
